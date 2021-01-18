0e725277baba43e130ca9a9b9961b96bf59864cd
Cory M <cmarker4@gmail.com>
2021-01-18 17:04:23 -0600

    commit tree 81807174169d1b4182a32f4ff4b1a4733810618e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actor, through: :character
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |i|
+      "#{i.actor.first_name} #{i.actor.last_name}"
+    end
+  end
 end
